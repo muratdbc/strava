904299258b7bd45f3a4bc5623fa01d0342985540
velveetachef <jeremysabat@gmail.com>
2014-12-02 13:49:48 -0800

    initial profile page update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def total_distance(user)
+    activities = user.activities.map { |activity| activity.distance }
+    activities.reduce(:+)
+  end
+
+  def total_distance_team(user_array)
+    totals = []
+    user_array.each do |user|
+      totals << total_distance(user)
+    end
+    totals.reduce(:+)
+  end
 end
