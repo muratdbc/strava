28176c6d8f10e2659ebc7b93e7bd411a0dda0762
Ethan Spiro <ethanspiro@gmail.com>
2014-12-02 13:33:22 -0800

    work on match controller and add css files

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,27 @@
 class GamesController < ApplicationController
+
+  def show
+
+    @placeholder = 'placeholder'
+    @arr =["one","two","three","four"]
+
+    @current_user_id = 1
+
+    @user = User.find(@current_user_id)
+    @team = @user.team_id
+
+    @game = Game.where(:team_id => @team).first || Game.where(:awayteam_id => @team).first
+
+    @home_team = Team.find(@game.team_id)
+    @away_team = Team.find(@game.awayteam_id)
+
+    p @home_team.users
+    p @away_team.users
+
+    render :index
+  end
+
+
+
+
 end
